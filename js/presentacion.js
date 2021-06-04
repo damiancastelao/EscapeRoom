3f8f7a478aab22811597e9d3ba7f8e82613ad91b
Damian Nogueiras <damiannogueiras@gmail.com>
2021-06-04 06:13:10 +0200

    todo en una página

--- js/presentacion.js
+++ js/presentacion.js
@@ -1,28 +1,41 @@
-var ano = "";
+var respuesta = "";
 var contador = 0;
 
 var myVideo = document.getElementById('my-video');
-var myMsg = document.getElementById('msg');
 
 var videos = ['videos/presentacionInsta.mp4','videos/reto.mp4'];
+var retoActual = 0;
+var respuestasCorrectas = ['3023','6666','5555','4444','3333'];
 var retoConseguido = ['0', '0'];
-var msgRetos = ['Introduce el año del futuro, desde donde viene el mensaje','El número de la bestia']
-
+var msgRetos = ['Introduce el año del futuro, desde donde viene el mensaje','El número de la bestia','wuow','guau','yeah'];
+//'<i class="fas fa-check"></i>';
+var checkReto = '<i class="fas fa-skull-crossbones"></i>';
 function darNumero(numero){
-    ano = ano + numero;
+    respuesta = respuesta + numero;
     contador++;
-    document.getElementById('respuesta').innerHTML = "<p>"+ano+"<p>";
-    console.log("Año: " + ano + " contador: " + contador);
-    if (contador==4 && ano=="3023") {
+    // rellenamos la respuesta segun vayan pulsando los otones
+    $('#respuesta'+contador)[0].innerText = numero;
+    console.log("Año: " + respuesta + " contador: " + contador);
+    if (contador>=4 && respuesta==respuestasCorrectas[retoActual]) {
         $('#feito').modal('show');
-        myVideo.setAttribute('src', videos[1])
-        myMsg.innerHTML = msgRetos[1];
+        // check
+        $('#reto'+retoActual)[0].innerHTML = checkReto;
+        // sumamos uno al reto
+        retoActual = retoActual + 1;
+        // cargamos el video nuevo
+        myVideo.setAttribute('src', videos[retoActual])
+        // texto del reto
+        $('#msg')[0].innerHTML = msgRetos[retoActual];
+        // contador y respuesta a cero
         contador = 0;
-        ano="";
-    } else if (contador==4 && ano!="3023"){
+        respuesta="";
+    } else if (contador>=4 && respuesta!=respuestasCorrectas[retoActual]){
         $('#nonfeito').modal('show');
         contador=0;
-        ano="";
-        document.getElementById('respuesta').innerText = "....";
+        respuesta="";
+        // reseteamos la respuesta con '?'
+        for(var i=1; i<5; i++) {
+            $('#respuesta'+i)[0].innerHTML='<i class="fas fa-question"></i>';
+        }
     }
 }
